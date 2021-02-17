ea86d34a26e21202224183a510348f37e698b3d2
Jaqueline115 <79179330+Jaqueline115@users.noreply.github.com>
2021-02-17 12:52:13 -0600

    Update envio.php

--- envio.php
+++ envio.php
@@ -5,7 +5,8 @@ $apellido_p = $_post["apellido paterno"];
 $apellido_m = $_post["apellido materno"];
 $fecha_n = $_post["fecha de nacimiento"];
 $genero = $_post["genero"];
+$profesion = $_post["profesion"];
 $ciudad = $_post["ciudad"];
-$DATOS = "nombre" .$nombre. "apellido_p".$apellido_p. "apellido_m" .$apellido_m. "fecha_n" .$fecha_n. "genero" .$genero. "ciudad" .$ciudad;
+$DATOS = "nombre" .$nombre. "apellido_p".$apellido_p. "apellido_m" .$apellido_m. "fecha_n" .$fecha_n. "genero" .$profesion. "profesion"; .$genero. "ciudad" .$ciudad;
 mail($destino, "Datos del Registro", $DATOS);
 ?>
